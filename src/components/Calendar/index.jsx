bb3860dff2791f07ffebe55fa96dce960c360a23
Roman Ratushnyi <remenok9@gmail.com>
2022-05-16 16:42:04 +0300

    Add scss

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import classNames from "classnames";
-import styles from "./Calendar.module.css";
+import styles from "./Calendar.module.scss";
 import {
   format,
   getDay,
